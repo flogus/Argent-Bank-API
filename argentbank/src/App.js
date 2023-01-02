77d84870e35fb5a375bf06c423a98bf283a27b57
flogus <fg@motionery.com>
2023-01-02 08:00:25 +0100

    feat: add components Footer, Hero and Section

--- argentbank/src/App.js
+++ argentbank/src/App.js
@@ -1,11 +1,19 @@
 import logo from './logo.svg';
 import './App.css';
-import Banner
- from './components/banner';
+import Banner from './components/banner';
+import Footer from './components/footer';
+import Hero from './components/hero';
+import Sections from './components/sections';
+
 function App() {
   return (
     <div className="App">
       <Banner />
+      <main>
+        <Hero />
+        <Sections />
+      </main>
+      <Footer />
     </div>
   );
 }
