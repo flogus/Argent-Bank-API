551d45003a67b8f9273305c3f2286d5457028120
flogus <fg@motionery.com>
2023-01-05 10:15:36 +0100

    feat: add link to new account

--- argentbank/src/pages/signin/signinpage.jsx
+++ argentbank/src/pages/signin/signinpage.jsx
@@ -1,7 +1,8 @@
 import React from 'react';
 import Banner from '../../components/banner';
 import Footer from '../../components/footer';
-import Form from './signinform'
+import Form from './signinform';
+import { Link } from "react-router-dom";
 
 function Signin() {
     return(
@@ -12,6 +13,7 @@ function Signin() {
                     <i className="fa fa-user-circle sign-in-icon"></i>
                     <h1>Sign In</h1>
                     <Form />
+                    <Link to="/create">Create a new account</Link>
                 </section>
             </main>
             <Footer />
