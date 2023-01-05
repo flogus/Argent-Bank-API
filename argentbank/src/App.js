1e6fdba3f9608bcd4fb1f46f53a70e15ba88f256
flogus <fg@motionery.com>
2023-01-05 10:27:19 +0100

    fix: correct the import of the profil page

--- argentbank/src/App.js
+++ argentbank/src/App.js
@@ -5,7 +5,7 @@ import Home from './pages/home/homepage'
 import Signin from './pages/signin/signinpage'
 import Transactions from './pages/transactions/transactionspage'
 import Create from './pages/create/createpage'
-import Profil from './pages/create/profil'
+import Profil from './pages/profil/profilpage'
 import { Routes, Route } from "react-router-dom"
 
 
