f815e2a7c901fe5fa868cdf5cdffa8242960d3a1
flogus <fg@motionery.com>
2023-01-06 09:09:38 +0100

    feat: add redux and reduxToolKit

--- argentbank/src/App.js
+++ argentbank/src/App.js
@@ -1,13 +1,10 @@
 import './App.css';
-
-
 import Home from './pages/home/homepage'
 import Signin from './pages/signin/signinpage'
 import Transactions from './pages/transactions/transactionspage'
 import Create from './pages/create/createpage'
 import Profil from './pages/profil/profilpage'
 import { Routes, Route } from "react-router-dom"
-
 
 function App() {
   return (
