ad7cc13ee3ba9642b13c49bb8d3a81b76bff6426
flogus <fg@motionery.com>
2023-01-10 09:34:18 +0100

    feat: add provider and the store file

--- argentbank/src/store.js
+++ argentbank/src/store.js
@@ -1,5 +1,14 @@
 import { configureStore } from '@reduxjs/toolkit'
 
+// function tasksReducer (tasks, action) {
+//   switch (action.type) {
+//     default: {
+//       throw Error('Unknown action: ' + action.type)
+//     }
+//     case 'addUser': {
+//     }
+//   }
+// }
 export default configureStore({
   reducer: {}
 })
