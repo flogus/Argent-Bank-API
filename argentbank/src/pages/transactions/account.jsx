832affeaf026b64b19a29ee89316ad8d66880a71
flogus <fg@motionery.com>
2023-01-12 10:00:59 +0100

    fix: add key attribut for the section

--- argentbank/src/pages/transactions/account.jsx
+++ argentbank/src/pages/transactions/account.jsx
@@ -1,18 +1,18 @@
-import React from 'react';
+import React from 'react'
 
-function Account(props) {
-    return (
-        <section className="account">
-            <div className="account-content-wrapper">
-                <h3 className="account-title">Argent Bank {props.title}</h3>
-                <p className="account-amount">${props.amount}</p>
-                <p className="account-amount-description">{props.text}</p>
-            </div>
-            <div className="account-content-wrapper cta">
-                <button className="transaction-button">View transactions</button>
-            </div>
-        </section>
-    );
+function Account (props) {
+  return (
+    <section className='account' key={props.amount}>
+      <div className='account-content-wrapper'>
+        <h3 className='account-title'>Argent Bank {props.title}</h3>
+        <p className='account-amount'>${props.amount}</p>
+        <p className='account-amount-description'>{props.text}</p>
+      </div>
+      <div className='account-content-wrapper cta'>
+        <button className='transaction-button'>View transactions</button>
+      </div>
+    </section>
+  )
 }
 
-export default Account;
+export default Account
