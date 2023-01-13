333bf67511e318b2a953e893edf8f187f9fb4d34
flogus <fg@motionery.com>
2023-01-13 14:54:21 +0100

    feat: add profil page

---
+++ argentbank/src/pages/profile/profileform.jsx
@@ -0,0 +1,66 @@
+import React, { useState } from 'react'
+import { Navigate, useNavigate } from 'react-router-dom'
+
+function Profileform () {
+  const [lastName, setLastName] = useState('')
+  const [firstName, setFirstName] = useState('')
+  const [email, setEmail] = useState('')
+  const [password, setPassword] = useState('')
+  const [message, setMessage] = useState('Please fill the form')
+
+  const signupUrl = 'http://localhost:3001/api/v1/user/signup'
+  const navigate = useNavigate()
+
+  const handleSubmit = async e => {
+    e.preventDefault()
+  }
+
+  return (
+    <form onSubmit={handleSubmit} method='post'>
+      <h3 className='text-sm'>{message}</h3>
+      <div className='input-wrapper'>
+        <label htmlFor='firstName'>First Name</label>
+        <input
+          type='text'
+          id='firstName'
+          value={firstName}
+          onChange={e => setFirstName(e.target.value)}
+        />
+      </div>
+      <div className='input-wrapper'>
+        <label htmlFor='lastName'>Last Name</label>
+        <input
+          type='text'
+          id='lastName'
+          value={lastName}
+          onChange={e => setLastName(e.target.value)}
+        />
+      </div>
+      <div className='input-wrapper'>
+        <label htmlFor='password'>Password</label>
+        <input
+          type='password'
+          id='password'
+          value={password}
+          onChange={e => setPassword(e.target.value)}
+        />
+      </div>
+      <div className='input-wrapper'>
+        <label htmlFor='email'>Email</label>
+        <input
+          type='text'
+          id='email'
+          value={email}
+          onChange={e => setEmail(e.target.value)}
+        />
+      </div>
+      <div className='input-remember'>
+        <input type='checkbox' id='remember-me' />
+        <label htmlFor='remember-me'>Remember me</label>
+      </div>
+      <button className='sign-in-button'>Create account</button>
+    </form>
+  )
+}
+
+export default Profileform
--- argentbank/src/pages/profile/profilepage.jsx
+++ argentbank/src/pages/profile/profilepage.jsx
@@ -1,20 +1,22 @@
-import React from 'react';
-import Banner from '../../components/banner';
-import Footer from '../../components/footer';
+import React from 'react'
+import Banner from '../../components/banner'
+import Footer from '../../components/footer'
+import Profileform from './profileform'
 
-function Profil() {
-    return(
-        <div className="App">
-            <Banner />
-            <main className="main bg-dark">
-                <section className="sign-in-content">
-                    <i className="fa fa-user-circle sign-in-icon"></i>
-                    <h1>Profil</h1>
-                </section>
-            </main>
-            <Footer />
-        </div>
-    )
-  }
-  
-  export default Profil;
+function Profile () {
+  return (
+    <div className='App'>
+      <Banner />
+      <main className='main bg-dark'>
+        <section className='sign-in-content'>
+          <i className='fa fa-user-circle sign-in-icon'></i>
+          <h1>Profile</h1>
+          <Profileform />
+        </section>
+      </main>
+      <Footer />
+    </div>
+  )
+}
+
+export default Profile
