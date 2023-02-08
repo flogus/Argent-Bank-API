ed0560253b8ce43d3f9c260a33c0b49fbb3e97f1
flogus <fg@motionery.com>
2023-02-08 11:23:45 +0100

    refactor: remove useless code

--- argentbank/src/features/user/Userinfo.js
+++ argentbank/src/features/user/Userinfo.js
@@ -1,12 +1,9 @@
-import { useState } from 'react'
 import { useSelector } from 'react-redux'
 
 import React from 'react'
 
 const Userinfo = () => {
   const user = useSelector(state => state.user)
-  const [userFirstname, useUserFisrtname] = useState(user.firstName)
-  console.log(userFirstname)
 
   return (
     <div>
