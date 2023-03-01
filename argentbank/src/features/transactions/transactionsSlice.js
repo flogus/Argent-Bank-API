e24235392103bcbab1214c86a08660d3fcf7d4b8
flogus <fg@motionery.com>
2023-03-01 16:12:18 +0100

    refactor: remove commented code

--- argentbank/src/features/transactions/transactionsSlice.js
+++ argentbank/src/features/transactions/transactionsSlice.js
@@ -1,17 +1,34 @@
 import { createAction, createSlice } from '@reduxjs/toolkit'
 
 const initialState = {
-  amount: 2082.79,
-  title: 'Checking (x8349)',
-  text: 'Available Balance'
+  amount: 1000,
+  title: 'Initial State',
+  text: 'Initial Balance'
 }
+// const initialState = {
+//   transac1: {
+//     amount: 1000,
+//     title: 'Initial State',
+//     text: 'Initial Balance'
+//   },
+//   transac2: {
+//     amount: 1000,
+//     title: 'Initial State',
+//     text: 'Initial Balance'
+//   },
+//   transac3: {
+//     amount: 1000,
+//     title: 'Initial State',
+//     text: 'Initial Balance'
+//   }
+// }
 
 const transactionSlice = createSlice({
   name: 'transactions',
   initialState: initialState,
   reducers: {
     initTransac (state) {
-      state.amount = '2082.79'
+      state.amount = 3000
       state.title = 'Checking (x8349)'
       state.text = 'Available Balance'
     },
